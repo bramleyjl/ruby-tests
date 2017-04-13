f03181ff299b78278d8f6498e81f62b339fda89b
John <bramleyjl@gmail.com>
2017-04-14 00:02:12 +0800

    Updated to make files accessible

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
-#write your code here
+def ftoc(temperature)
+		convert = 5/9.to_f
+		(temperature.to_f-32.0)*convert
+end
+
+def ctof(temperature)
+	convert = 9/5.to_f
+	(temperature.to_f*convert)+32
+end
+
+print ctof(0)
