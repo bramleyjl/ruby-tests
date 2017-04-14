2534849d1505d43a33a17ff26fde177de949d1eb
bramleyjl <bramleyjl@gmail.com>
2017-04-14 15:22:40 +0800

    test 03_simon_says completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,49 @@
-#write your code here
+def echo(input)
+	input
+end
+
+def shout(input)
+	input.upcase
+end
+
+def repeat(input, frequency = 2)
+	simon = ""
+	while frequency > 1
+		simon = simon + input + " "
+		frequency -= 1
+	end
+	if frequency == 1
+		simon = simon + input
+	end
+	simon
+end
+
+def start_of_word(input, letters)
+	input[0...letters]
+end
+
+def first_word(input)
+	list = input.split(" ")
+	list[0]
+end
+
+def titleize(input)
+	list = input.split(" ")
+	
+	list.each do |word|
+	
+		if word == list[0]
+			word.capitalize!
+
+		elsif word == "over" || word == "the" || word == "and"
+			word.downcase!
+		
+		else
+			word.capitalize!
+		end
+	end
+
+	list.join(" ")
+end
+
+	
